e470a2ec7edea0bf0b670ac327f9b33aa5d3342b
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-02-06 22:36:09 +0530

    Update decrypt function parameter name

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func encrypt(key int ,plainText string)(result string){
 	
 }
 
-func decrypt(key int ,plainText string){}
+func decrypt(key int ,encryptedText string){}
 func main() {
 	plainText := "HELLOWORLD"
 	fmt.Println("Plain Text : ", plainText)

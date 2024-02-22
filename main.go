57f6c606a73645713fa963451e963d0096fdedae
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-02-22 16:24:10 +0530

    Add encrypt and decrypt functions

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import "fmt"
 
 func encrypt(key int, plainText string) (result string) {
 
+
+
+
+
 }
 
 func decrypt(key int, encryptedText string) (result string) {
+
+
+
 
 }
 
@@ -16,4 +23,9 @@ func main() {
 	encrypted := encrypt(5, plainText)
 
 	fmt.Printf("Encrypted Text",encrypted)
+
+
+
+	decrypted:=decrypt(5,encrypted)
+	fmt.Printf(decrypted)
 }
